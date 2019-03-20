cb8382cfc167d198a52ffab26ba4ddde498830e0
greencar77 <karlisc@inbox.lv>
2019-03-20 17:40:46 +0200

    favicon

--- core/src/main/java/com/rabarbers/call/publish/TracePublisher.java
+++ core/src/main/java/com/rabarbers/call/publish/TracePublisher.java
@@ -2,6 +2,7 @@ package com.rabarbers.call.publish;
 
 import com.rabarbers.call.domain.Trace;
 import com.rabarbers.call.domain.call.Call;
+import com.rabarbers.call.html.Link;
 import com.rabarbers.call.html.page.HtmlPage;
 import org.apache.commons.lang3.StringUtils;
 
@@ -11,9 +12,10 @@ public abstract class TracePublisher extends HtmlPublisher {
     public static final String FILLED_TAB = "|" + StringUtils.repeat("-", TAB_SIZE);
 
     public void publish(Trace trace) {
-        HtmlPage root = new HtmlPage("T: " + trace.getName());
+        HtmlPage root = new HtmlPage(trace.getName());
         appendStylesheets(root);
         appendScripts(root);
+        appendFavicon(root);
 
         StringBuilder sb = new StringBuilder();
         appendTraceDetails(sb, trace);
@@ -29,6 +31,14 @@ public abstract class TracePublisher extends HtmlPublisher {
     protected abstract void appendTraceDetails(StringBuilder sb, Trace trace);
 
     protected String getMethod(Call call) {
-        return call.getMethod().getName();
+        return "<span class=\"method\" title=\"" + call.getMethod().getSignatureX() + "\">" + call.getMethod().getName() + "</span>";
+    }
+
+    protected void appendFavicon(HtmlPage root) {
+        Link link = new Link();
+        link.setAttr("rel", "shortcut icon");
+        link.setAttr("type", "image/png");
+        link.setAttr("href", "../" + COMMON_FOLDER + "favicon_t.ico");
+        root.getHead().appendChild(link);
     }
 }
